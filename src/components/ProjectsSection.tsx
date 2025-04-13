7dbbfea213aa7c7abe112c59164a6768672ac3e9
Battam1111 <1035043941@qq.com>
2025-04-13 19:27:11 +0800

    update

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,8 +1,15 @@
+import projects from "@/data/projects";
+import ProjectCard from "./ProjectCard";
+
 export default function ProjectsSection() {
-    return (
-      <section>
-        <h2>Projects Placeholder</h2>
-      </section>
-    );
-  }
-  
+  return (
+    <section className="space-y-8" id="projects">
+      <h2 className="text-3xl font-semibold border-b pb-2">Projects</h2>
+      <div className="grid md:grid-cols-2 gap-8">
+        {projects.map((project) => (
+          <ProjectCard key={project.title} {...project} />
+        ))}
+      </div>
+    </section>
+  );
+}
